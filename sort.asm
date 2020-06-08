e8d5d0f67568f8bb56405a9de3386978f88b0129
Valencia <davidalencia@gmail.com>
2020-06-07 21:40:48 -0500

    ya funciona bubble sort

--- sort.asm
+++ sort.asm
@@ -3,29 +3,35 @@
 	n:	.asciiz "\n"
 .text
 
+#lee int
 .macro read_int(%in)
 	li $v0, 5
 	syscall
 	move %in $v0
 .end_macro 
 
+#imprime cadena de registro
 .macro print_str(%r)
 	li $v0 4
 	la $a0, %r 
     	syscall
 .end_macro
 
+#imprime int
 .macro print_int(%out)
 	li $v0 1
 	move $a0, %out  
     	syscall
 .end_macro
 
+
+#es lo mismo que %v = %arr[%i]
 .macro get (%arr, %i, %v)	
 	add $t6 $zero %i
 	mul $t7 $t6 8
 	lw %v %arr($t7)
 .end_macro
+
 
 # generic looping mechanism
 .macro for (%regIterator, %from, %to, %label)
@@ -37,14 +43,15 @@
 .end_macro
 
 
-j main
-#definitions
+j main #brinco a main para saltarse las definiciones 
+
+#definiciones
 outer_loop: 
 	subi	$sp $sp 32 	# paso1: reservar espacio(no hay paso2)
 	sw	$ra 16($sp)	# paso3: guardar direccion de retorno
 	sw	$fp 20($sp)	# paso4: guardar fp
 	
-	for($t4, $t0, $t1, inner_loop)
+	for($t1, $t0, $s0, inner_loop) #for desde t0 hasta s0, el cuerpo es inner_loop
 
 	lw	$ra 16($sp)
 	lw	$fp 20($sp)
@@ -55,47 +62,47 @@ inner_loop:
 	subi	$sp $sp 32 	# paso1: reservar espacio(no hay paso2)
 	sw	$ra 16($sp)	# paso3: guardar direccion de retorno
 	sw	$fp 20($sp)	# paso4: guardar fp
+		
+	get(arr, $t0, $t2)
+	get(arr, $t1, $t3)
 	
-	get(arr, $t0, $t5)
-	print_int($t5)
-	get(arr, $t4, $t5)
-	print_int($t5)
-	print_str(n)
+	ble $t2 $t3  if_end #si t1->t3 es menor que t0-t2 cambialos
+	
+	mul $t4 $t0 8 # calculamos el indice de guardado
+	sw $t3 arr($t4) #lo guardamos en el arreglo
+	mul $t4 $t1 8 # calculamos el indice de guardado
+	sw $t2 arr($t4) #lo guardamos en el arreglo
+
+if_end:
 	
 	lw	$ra 16($sp)
 	lw	$fp 20($sp)
 	addi	$sp $sp	32
 	jr	$ra
+	
+read_int: 
+	read_int($t1) #leemos int
+	mul $t2 $t0 8 # calculamos el indice de guardado
+	sw $t1 arr($t2) #lo guardamos en el arreglo
+	
+	jr	$ra
+
+print_int:
+	get(arr, $t0, $t1)
+	print_int($t1)
+	print_str(n)
+	jr	$ra
 
 main:
 
-read_int($t1)
+read_int($s0) # leemos el tamaño del arreglo
 
-addi $t2 $zero 0
-read_int: beq $t1 $t2 start_ordering
-	read_int($s0)
-	mul $t3 $t2 8
-	sw $s0 arr($t3)
-	add $t2 $t2 1
-	j read_int
+for($t0, 0, $s0, read_int)
+
+for($t0, 0, $s0, outer_loop) # iniciamos el  for loop con el outer_loop de cuerpo
+for($t0, 0, $s0, print_int)
 	
 
-
-start_ordering: addi $t2 $zero 0
-		addi $t3 $zero 0
-ordering:
-	for($t0, 0, $t1, outer_loop)
-	print_int($t0)
-	
-#	beq $t1 $t2 end
-#	j inner_loop
-#	addi $t2 $t2 1
-#	
-#inner_loop: beq $t1 $t3 $ra
-#	addi $t3 $t3 1
-#	
-#
-#end
 
 
 
